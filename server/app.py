28dbd5f49831a0a2ef2299536923b39b06ccf4b7
Ej Myles <ejelymyles@gmail.com>
2024-03-31 19:16:59 -0700

    finish lab

--- server/app.py
+++ server/app.py
@@ -14,13 +14,49 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
 
-@app.route('/messages/<int:id>')
+    if request.method == 'GET':
+        messages = Message.query.order_by(Message.created_at.asc()).all()
+        messages_dict = [message.to_dict() for message in messages]
+        return jsonify(messages_dict), 200
+    
+    elif request.method == 'POST':
+        request_data = request.json
+        new_message = Message(
+            body=request_data.get("body"),
+            username=request_data.get("username"),
+        )
+
+        db.session.add(new_message)
+        db.session.commit()
+        new_message_dict = new_message.to_dict()
+        return jsonify(new_message_dict), 201
+    
+
+@app.route('/messages/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id=id).first()
+
+    if request.method == 'GET':
+        return make_response(message.to_dict(), 200)
+
+    elif request.method == 'PATCH':
+        request_data = request.json
+        for attr, value in request_data.items():
+            setattr(message, attr, value)
+        
+        db.session.commit()
+        message_dict = message.to_dict()
+        return jsonify(message_dict), 200
+
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+        return make_response({'message':'record successfully deleted'}, 200)
+
+    
 
 if __name__ == '__main__':
     app.run(port=5555)
---
+++ server/migrations/versions/6bc050f1c967_your_message.py
@@ -0,0 +1,24 @@
+"""your message
+
+Revision ID: 6bc050f1c967
+Revises: 
+Create Date: 2024-03-25 23:39:00.644273
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6bc050f1c967'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
